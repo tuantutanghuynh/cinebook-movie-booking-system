59f0cae1ee28d088a4fdadb3595c155f98103d34
TuanTu <tanghuynhtuantuwork@gmail.com>
2026-04-21 15:12:52 +0700

    feat: add Axios interceptor and protectedrout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,21 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <div>Profile Page (comming soon)</div>
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
